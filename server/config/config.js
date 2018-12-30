d5cfa2a0233780eb24605aa185c3785fc64895c2
esantiagohz <esantiagohz@gmail.com>
2018-12-30 00:33:19 -0600

    variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb://esantiagohz:nugetmsk93@ds121674.mlab.com:21674/cafeesh';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
